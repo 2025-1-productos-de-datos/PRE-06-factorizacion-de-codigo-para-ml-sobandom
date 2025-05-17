ccdd74b7cb32eb005d80095a0ae9301090acd0e8
sobandom <sobandom@unal.edu.co>
2025-05-17 12:44:34 -0500

    avance

--- homework/train_knn.py
+++ homework/train_knn.py
@@ -11,19 +11,30 @@ from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsRegressor
 
+from homework.calculate_metrics import calculate_metrics
+
+
 # descarga de datos
-url = "http://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-red.csv"
-df = pd.read_csv(url, sep=";")
+def prepare_data(file_path, test_size, random_state):
 
-# preparacion de datos
-y = df["quality"]
-x = df.copy()
-x.pop("quality")
+    df = pd.read_csv(file_path)
 
-# dividir los datos en entrenamiento y testing
-(x_train, x_test, y_train, y_test) = train_test_split(
-    x,
-    y,
+    y = df["quality"]
+    x = df.copy()
+    x.pop("quality")
+
+    (x_train, x_test, y_train, y_test) = train_test_split(
+        x,
+        y,
+        test_size=test_size,
+        random_state=random_state,
+    )
+
+    return x_train, x_test, y_train, y_test
+
+
+x_train, x_test, y_train, y_test = prepare_data(
+    file_path="data/winequality-red.csv",
     test_size=0.25,
     random_state=123456,
 )
@@ -35,11 +46,7 @@ estimator.fit(x_train, y_train)
 print()
 print(estimator, ":", sep="")
 
-# Metricas de error durante entrenamiento
-y_pred = estimator.predict(x_train)
-mse = mean_squared_error(y_train, y_pred)
-mae = mean_absolute_error(y_train, y_pred)
-r2 = r2_score(y_train, y_pred)
+mse, mae, r2 = calculate_metrics(x_train, y_train, estimator)
 
 print()
 print("Metricas de entrenamiento:")
