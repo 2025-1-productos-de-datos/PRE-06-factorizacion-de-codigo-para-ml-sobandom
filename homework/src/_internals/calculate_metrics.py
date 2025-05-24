192b320f0988a533aa86d114e5f0592e638f3779
sobandom <sobandom@unal.edu.co>
2025-05-24 08:45:51 -0500

    cuarto intento

--- homework/src/_internals/calculate_metrics.py
+++ homework/src/_internals/calculate_metrics.py
@@ -1,8 +1,8 @@
-# Metricas de error durante entrenamiento
+# Metricas de error durante testing
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 
 
-def calculate_metrics(x, y, model):
+def calculate_metrics(model, x, y):
     y_pred = model.predict(x)
     mse = mean_squared_error(y, y_pred)
     mae = mean_absolute_error(y, y_pred)
